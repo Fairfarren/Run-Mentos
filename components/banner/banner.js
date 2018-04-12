394bdabd75e28e4224b36077a53f3a0af9696b81
Yanke Liu <lykwow@live.com>
2018-04-12 23:45:57 +0800

    banner组件

---
+++ components/banner/banner.js
@@ -0,0 +1,23 @@
+// components/banner/banner.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    bannerList: Array
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    list: [1,2,3]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    
+  }
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,24 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+    banner: [{
+      img: 'https://i.loli.net/2018/03/25/5ab75a03c5ee2.jpeg',
+      path: 'https://www.baidu.com'
+    }, {
+      img: 'https://i.loli.net/2018/03/25/5ab75a0538920.jpeg',
+      path: '/card'
+    }, {
+      img: 'https://i.loli.net/2018/03/25/5ab75a0f40956.jpeg',
+      path: '/hello'
+    }, {
+      img: 'https://i.loli.net/2018/03/25/5ab75a130047b.jpg',
+      path: '/home'
+    }, {
+      img: 'https://i.loli.net/2018/03/25/5ab75a1351249.jpeg',
+      path: 'http://www.google.com'
+    }, {
+      img: 'https://i.loli.net/2018/03/25/5ab75a13f22d6.jpg',
+      path: 'asdfasdf'
+    }]
   }
 })
